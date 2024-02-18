6b65567dc79dd9d409eb96065c3364450011ff90
nemo <nemo@nemo.nemo>
2024-02-18 16:50:25 +0300

    v0.2

--- habr_blocklist.js
+++ habr_blocklist.js
@@ -1,4 +1,3 @@
-// ==UserScript==
 // @name           blacklist habr
 // @namespace      habr
 // @version        1.1
@@ -7,6 +6,24 @@
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
 // @icon           http://habr.com/favicon.ico
 // @grant          none
+// ==/UserScript==
+
+var blacklist=[];
+// populate blacklist with you authors
+blacklist.push('author');
+const articles = document.querySelectorAll('article');
+for(const article of articles){
+    var author = article.getElementsByClassName('tm-user-info__userpic')[0];
+    var name = author.attributes.getNamedItem('title// ==UserScript==
+// @name           blacklist habr
+// @namespace      S1efr1ed
+// @version        1.1
+// @description    Clear the main page of habr.com from blacklisted authors
+// @match          https://habr.com/*
+// @require        http://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
+// @icon           http://habr.com/favicon.ico
+// @grant          none
+// @license        MIT
 // ==/UserScript==
 
 var blacklist=[];
